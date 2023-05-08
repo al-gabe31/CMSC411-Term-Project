b11e160f44905dc42c36ef4048325305ee48b66d
Gabe Aquino <lq90575@umbc.edu>
2023-05-07 22:43:58 -0400

    Added bitwise AND OR operations

--- Register.py
+++ Register.py
@@ -38,3 +38,55 @@ class Register:
 
     def __eq__(self, otherRegister):
         return self.data == otherRegister.data
+
+# Returns value from a bitwise AND
+def bitwise_AND(reg1, reg2):
+    bits = [0 for i in range(NUM_BITS_IN_REG)]
+    
+    for i in range(NUM_BITS_IN_REG):
+        if reg1.bits[i] == 1 and reg2.bits[i] == 1:
+            bits[i] = 1
+    
+    value = 0
+    
+    if bits[0] == 1:
+        value -= pow(2, NUM_BITS_IN_REG - 1)
+    
+    for i in range(1, NUM_BITS_IN_REG):
+        if bits[i] == 1:
+            value += pow(2, NUM_BITS_IN_REG - i - 1)
+    
+    return value
+
+# Returns value from a bitwise ANDI
+def bitwise_ANDI(reg1, immediate):
+    reg2 = Register()
+    reg2.insert_data(immediate)
+    
+    return bitwise_AND(reg1, reg2)
+
+# Returns value from a bitwise OR
+def bitwise_OR(reg1, reg2):
+    bits = [0 for i in range(NUM_BITS_IN_REG)]
+    
+    for i in range(NUM_BITS_IN_REG):
+        if reg1.bits[i] == 1 or reg2.bits[i] == 1:
+            bits[i] = 1
+    
+    value = 0
+    
+    if bits[0] == 1:
+        value -= pow(2, NUM_BITS_IN_REG - 1)
+    
+    for i in range(1, NUM_BITS_IN_REG):
+        if bits[i] == 1:
+            value += pow(2, NUM_BITS_IN_REG - i - 1)
+    
+    return value
+
+# Returns value from a bitwise ORI
+def bitwise_ORI(reg1, immediate):
+    reg2 = Register()
+    reg2.insert_data(immediate)
+    
+    return bitwise_OR(reg1, reg2)
